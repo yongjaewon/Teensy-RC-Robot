5e998cb621e7914cfa091feaa0b7007b11e7f34e
Won, Yongjae <ywon30@gatech.edu>
2016-12-22 04:39:37 -0500

    removed failsafe code. removed RGB LED code. Defined channels to make it easier to identify.

--- Teensy-RC-Robot/Teensy-RC-Robot.ino
+++ Teensy-RC-Robot/Teensy-RC-Robot.ino
@@ -6,18 +6,22 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-const int teensyLED = 13;
+//const int teensyLED = 13;
 
 const int buzzer = 12;
 unsigned long buzzerPreviousMillis = 0;
 unsigned long buzzerInterval = 200;
 bool buzzerOn = false;
 
-const int redPin = 21;
-const int greenPin = 23;
-const int bluePin = 22;
-unsigned long rgbPreviousMillis = 0;
-bool rgbOn = false;
+//const int redPin = 21;
+//const int greenPin = 22;
+//const int bluePin = 23;
+//unsigned long rgbPreviousMillis = 0;
+//bool rgbOn = false;
+
+const int switchUp = 1904;
+const int switchMiddle = 1024;
+const int switchDown = 144;
 
 unsigned long batteryAlarmPreviousMillis = 0;
 unsigned long batteryResetPreviousMillis = 0;
@@ -39,6 +43,11 @@ int batteryVoltage;
 
 const int failsafeChannels[] = {1023, 1023, 1023, 1023, 1024, 1904, 1024, 1024};
 
+//gymbals horizontal: 366(left)  1023(center) 1680(right)
+//gymbals vertical: 366(top) 1023(center) 1680(bottom)
+//KEEP IN MIND: gymbals are analog and do not center perfectly every time
+//Switches: 1904 
+
 FUTABA_SBUS sBus;
 
 // hardware SPI on Teensy 3.x: MOSI pin 11, SCK pin 13 plus the pins defined below
@@ -51,20 +60,27 @@ RoboClaw roboclaw(&Serial3, 10000);
 #define RC1 0x80
 #define RC2 0x81
 
+#define ch1 sBus.channels[0]
+#define ch2 sBus.channels[1]
+#define ch3 sBus.channels[2]
+#define ch4 sBus.channels[3]
+#define ch5 sBus.channels[4]
+#define ch6 sBus.channels[5]
+#define ch7 sBus.channels[6]
+#define ch8 sBus.channels[7]
+
 void setup(){
-  pinMode(teensyLED, OUTPUT);
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
+//  pinMode(teensyLED, OUTPUT);
+//  pinMode(redPin, OUTPUT);
+//  pinMode(greenPin, OUTPUT);
+//  pinMode(bluePin, OUTPUT);
   pinMode(buzzer, OUTPUT);
   
   sBus.begin();
   Serial.begin(115200);
   roboclaw.begin(38400);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3D);
-  failSafe();
-  drive();
-  delay(500);
+  initiallizeChannels();
 }
 
 void loop(){
@@ -74,44 +90,91 @@ void loop(){
     sBus.UpdateServos();
     sBus.UpdateChannels();
     sBus.toChannels = 0;
+    normalizeGimbals();
   }
 
-  if (!radioStatus()) {
-    failSafe();
-  }
+//  if (!radioStatus()) {
+//    failSafe();
+//  }
   
   drive();
   updateIndicators();
   updateDisplay();
 }
 
+void normalizeGimbals() {
+  ch1 = 2048 - ch1;
+  ch4 = 2048 - ch4;
+  int temp = ch3;
+  ch3 = ch4;
+  ch4 = temp;
+  
+  ch1 = ch1 - 1024;
+  ch2 = ch2 - 1024;
+  ch3 = ch3 - 1024;
+  ch4 = ch4 - 1024;
+
+  deadband();
+}
+
+void deadband() {
+  if (ch1 > -20 && ch1 < 20) {
+    ch1 = 0;
+  }
+  if (ch2 > -20 && ch2 < 20) {
+    ch2 = 0;
+  }
+  if (ch3 > -20 && ch3 < 20) {
+    ch3 = 0;
+  }
+  if (ch4 > -20 && ch4 < 20) {
+    ch4 = 0;
+  }
+}
+
 void updateDisplay() {
   unsigned long currentMillis = millis();
-  if (sBus.channels[5] == 144) { //display 1
+  if (ch6 == switchUp) {
     if (currentMillis - displayChannelsPreviousMillis >= displayChannelsRefreshInterval) {
       displayChannelsPreviousMillis = currentMillis;
       displayChannels();
     }
-  } else if (sBus.channels[5] == 1904) { //display 2
-    if (currentMillis - displayVoltagePreviousMillis >= displayVoltageRefreshInterval) {
-      displayVoltagePreviousMillis = currentMillis;
-      displayVoltage();
-    }
-  } else {
+  } else if (ch6 == switchMiddle) {
     if (currentMillis - displaySettingsPreviousMillis >= displaySettingsRefreshInterval) {
       displaySettingsPreviousMillis = currentMillis;
       displaySettings();
+    }
+  } else {
+    if (currentMillis - displayVoltagePreviousMillis >= displayVoltageRefreshInterval) {
+      displayVoltagePreviousMillis = currentMillis;
+      displayVoltage();
     }
   }
 }
 
 void failSafe() {
-  for (int i = 0; i < 8; i++) {
-    sBus.channels[i] = failsafeChannels[i];
-  }
+//  sBus.channels[0] = failsafeChannels[0]; //ch1
+//  sBus.channels[1] = failsafeChannels[1]; //ch2
+//  sBus.channels[2] = failsafeChannels[2]; //ch3
+//  sBus.channels[3] = failsafeChannels[3]; //ch4
+//  sBus.channels[4] = failsafeChannels[4]; //ch5
+//  sBus.channels[5] = failsafeChannels[5]; //ch6
+//  sBus.channels[6] = failsafeChannels[6]; //ch7
+//  sBus.channels[7] = failsafeChannels[7]; //ch8
 }
 
-void buzz(bool condition) {
+void initiallizeChannels() {
+  ch1 = 0;
+  ch2 = 0;
+  ch3 = 0;
+  ch4 = 0;
+  ch5 = switchMiddle;
+  ch6 = switchDown;
+  ch7 = switchMiddle;
+  ch8 = switchMiddle;
+}
+
+void setBuzzer(bool condition) {
   if (condition) {
     unsigned long currentMillis = millis();
     if (currentMillis - buzzerPreviousMillis >= buzzerInterval) {
@@ -124,7 +187,7 @@ void buzz(bool condition) {
         buzzerOn = true;
       }
     }
-  } else if (sBus.channels[7] == 144){
+  } else if (ch8 == switchUp){
     unsigned long currentMillis = millis();
     if (currentMillis - buzzerPreviousMillis >= buzzerInterval) {
       buzzerPreviousMillis = currentMillis;
@@ -143,26 +206,26 @@ void buzz(bool condition) {
 
 void updateIndicators() {
   if (batteryStatus()) {
-    buzz(false);
-    if (radioStatus()) {
-      setRGB(0, 255, 0, 0);
-    } else {
-      setRGB(0, 255, 0, 500);
-    }
+    setBuzzer(false);
+//    if (radioStatus()) {
+//      setRGB(0, 255, 0, 0);
+//    } else {
+//      setRGB(0, 255, 0, 500);
+//    }
   } else if (!roboclawConnected) {
-    buzz(false);
-    if (radioStatus()) {
-      setRGB(40, 255, 0, 0);
-    } else {
-      setRGB(40, 255, 0, 500);
-    }
+    setBuzzer(false);
+//    if (radioStatus()) {
+//      setRGB(40, 255, 0, 0);
+//    } else {
+//      setRGB(40, 255, 0, 500);
+//    }
   } else {
-    buzz(true);
-    if (radioStatus()) {
-      setRGB(255, 0, 0, 0);
-    } else {
-      setRGB(255, 0, 0, 500);
-    }
+    setBuzzer(true);
+//    if (radioStatus()) {
+//      setRGB(255, 0, 0, 0);
+//    } else {
+//      setRGB(255, 0, 0, 500);
+//    }
   }
 }
 
@@ -174,8 +237,12 @@ bool radioStatus() {
   }
 }
 
-bool batteryStatus() {
+void updateBatteryVoltage() {
   batteryVoltage = roboclaw.ReadMainBatteryVoltage(RC1, &roboclawConnected);
+}
+
+bool batteryStatus() {
+  updateBatteryVoltage();
   unsigned long currentMillis = millis();
   if (!roboclawConnected) {
     return false;
@@ -197,7 +264,7 @@ bool batteryStatus() {
 }
 
 void drive() {
-  int drive = (int)((sBus.channels[1] - 1023) / 5.17);
+  int drive = (int)(ch2 / 5.16);
   if (drive >= 0) {
     roboclaw.ForwardM1(RC2, drive);
   } else {
@@ -205,29 +272,29 @@ void drive() {
   }
 }
 
-void setRGB(int red, int green, int blue, unsigned long blinkInterval)
-{
-  unsigned long currentMillis = millis();
-  
-  if (blinkInterval == 0) {
-    analogWrite(redPin, red);
-    analogWrite(greenPin, green);
-    analogWrite(bluePin, blue);
-  } else if (currentMillis - rgbPreviousMillis >= blinkInterval) {
-    rgbPreviousMillis = currentMillis;
-    if (rgbOn) {
-      analogWrite(redPin, 0);
-      analogWrite(greenPin, 0);
-      analogWrite(bluePin, 0);
-      rgbOn = false;
-    } else {
-      analogWrite(redPin, red);
-      analogWrite(greenPin, green);
-      analogWrite(bluePin, blue);
-      rgbOn = true;
-    }
-  }
-}
+//void setRGB(int red, int green, int blue, unsigned long blinkInterval)
+//{
+//  unsigned long currentMillis = millis();
+//  
+//  if (blinkInterval == 0) {
+//    analogWrite(redPin, red);
+//    analogWrite(greenPin, green);
+//    analogWrite(bluePin, blue);
+//  } else if (currentMillis - rgbPreviousMillis >= blinkInterval) {
+//    rgbPreviousMillis = currentMillis;
+//    if (rgbOn) {
+//      analogWrite(redPin, 0);
+//      analogWrite(greenPin, 0);
+//      analogWrite(bluePin, 0);
+//      rgbOn = false;
+//    } else {
+//      analogWrite(redPin, red);
+//      analogWrite(greenPin, green);
+//      analogWrite(bluePin, blue);
+//      rgbOn = true;
+//    }
+//  }
+//}
 
 void displayChannels() {
   display.clearDisplay();
