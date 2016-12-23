90efdb1cdb102bb1d052aaa9b47e3a20df06d9e0
Won, Yongjae <ywon30@gatech.edu>
2016-12-23 04:06:44 -0500

    removed unused code that was previously commented out. normalizing channels no longer modifies the sBus.channels. It creates a copy and then normalizes the copy. added a counter with reading sBus data because sBus outputs some weird data at initial startup. the counter ignores the first 10 packets from sBus.

--- Teensy-RC-Robot/Teensy-RC-Robot.ino
+++ Teensy-RC-Robot/Teensy-RC-Robot.ino
@@ -6,22 +6,15 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-//const int teensyLED = 13;
-
 const int buzzer = 12;
 unsigned long buzzerPreviousMillis = 0;
-unsigned long buzzerInterval = 200;
 bool buzzerOn = false;
-
-//const int redPin = 21;
-//const int greenPin = 22;
-//const int bluePin = 23;
-//unsigned long rgbPreviousMillis = 0;
-//bool rgbOn = false;
 
 const int switchUp = 1904;
 const int switchMiddle = 1024;
 const int switchDown = 144;
+
+const int gimbalCenter = 1024;
 
 unsigned long batteryAlarmPreviousMillis = 0;
 unsigned long batteryResetPreviousMillis = 0;
@@ -43,11 +36,6 @@ int batteryVoltage;
 
 const int failsafeChannels[] = {1023, 1023, 1023, 1023, 1024, 1904, 1024, 1024};
 
-//gymbals horizontal: 366(left)  1023(center) 1680(right)
-//gymbals vertical: 366(top) 1023(center) 1680(bottom)
-//KEEP IN MIND: gymbals are analog and do not center perfectly every time
-//Switches: 1904 
-
 FUTABA_SBUS sBus;
 
 // hardware SPI on Teensy 3.x: MOSI pin 11, SCK pin 13 plus the pins defined below
@@ -60,20 +48,20 @@ RoboClaw roboclaw(&Serial3, 10000);
 #define RC1 0x80
 #define RC2 0x81
 
-#define ch1 sBus.channels[0]
-#define ch2 sBus.channels[1]
-#define ch3 sBus.channels[2]
-#define ch4 sBus.channels[3]
-#define ch5 sBus.channels[4]
-#define ch6 sBus.channels[5]
-#define ch7 sBus.channels[6]
-#define ch8 sBus.channels[7]
+int channels[8];
+
+#define ch1 channels[0]
+#define ch2 channels[1]
+#define ch3 channels[2]
+#define ch4 channels[3]
+#define ch5 channels[4]
+#define ch6 channels[5]
+#define ch7 channels[6]
+#define ch8 channels[7]
+
+int aCounter = 0;
 
 void setup(){
-//  pinMode(teensyLED, OUTPUT);
-//  pinMode(redPin, OUTPUT);
-//  pinMode(greenPin, OUTPUT);
-//  pinMode(bluePin, OUTPUT);
   pinMode(buzzer, OUTPUT);
   
   sBus.begin();
@@ -81,54 +69,146 @@ void setup(){
   roboclaw.begin(38400);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3D);
   initiallizeChannels();
+  startupSound();
 }
 
 void loop(){
   sBus.FeedLine();
   if (sBus.toChannels == 1){
     radioInitialized = true;
-    sBus.UpdateServos();
-    sBus.UpdateChannels();
-    sBus.toChannels = 0;
-    normalizeGimbals();
+    //sBus outputs some weird data at the beginning. this counter ignores the first 10 packets.
+    if (aCounter > 10) {
+      sBus.UpdateServos();
+      sBus.UpdateChannels();
+      sBus.toChannels = 0;
+      normalizeChannels();
+    } else {
+      aCounter++;
+    }
   }
 
-//  if (!radioStatus()) {
-//    failSafe();
-//  }
-  
   drive();
+  updateBatteryVoltage();
   updateIndicators();
   updateDisplay();
 }
 
-void normalizeGimbals() {
+void drive() {
+  if (ch2 >= 0) {
+    roboclaw.ForwardM1(RC2, ch2);
+  } else {
+    roboclaw.BackwardM1(RC2, -ch2);
+  }
+}
+
+void startupSound() {
+  tone(buzzer, 698);
+  delay(100);
+  tone(buzzer, 932);
+  delay(100);
+  noTone(buzzer);
+}
+
+void initiallizeChannels() {
+  sBus.channels[0] = gimbalCenter;
+  sBus.channels[1] = gimbalCenter;
+  sBus.channels[2] = gimbalCenter;
+  sBus.channels[3] = gimbalCenter;
+  sBus.channels[4] = switchMiddle;
+  sBus.channels[5] = switchUp;
+  sBus.channels[6] = switchMiddle;
+  sBus.channels[7] = switchMiddle;
+}
+
+void normalizeChannels() {
+  for (int i = 0; i < 8; i++) {
+    channels[i] = sBus.channels[i];
+  }
+  
+  //reverse channels 1 and 4
   ch1 = 2048 - ch1;
   ch4 = 2048 - ch4;
+
+  //swap channels 3 and 4
   int temp = ch3;
   ch3 = ch4;
   ch4 = temp;
-  
-  ch1 = ch1 - 1024;
-  ch2 = ch2 - 1024;
-  ch3 = ch3 - 1024;
-  ch4 = ch4 - 1024;
 
-  deadband();
+  //for channels 1 through 4 (gimbals)
+  for (int i = 0; i < 4; i++) {
+    //zero the center
+    channels[i] -= 1024;
+
+    //downrate to 8-bit
+    channels[i] = (int)(channels[i] / 5.165);
+
+    //apply deadband
+    if (channels[i] >= -2 && channels[i] <= 2) {
+      channels[i] = 0;
+    }
+  }
 }
 
-void deadband() {
-  if (ch1 > -20 && ch1 < 20) {
-    ch1 = 0;
+void setBuzzer(int buzzerFrequency, unsigned long buzzerInterval) {
+  if (buzzerFrequency > 0) {
+    if (buzzerInterval == 0) {
+      tone(buzzer, buzzerFrequency);
+    } else {
+      unsigned long currentMillis = millis();
+      if (currentMillis - buzzerPreviousMillis >= buzzerInterval) {
+        buzzerPreviousMillis = currentMillis;
+        if (buzzerOn) {
+          noTone(buzzer);
+          buzzerOn = false;
+        } else {
+          tone(buzzer, buzzerFrequency);
+          buzzerOn = true;
+        }
+      }
+    }
+  } else {
+    noTone(buzzer);
   }
-  if (ch2 > -20 && ch2 < 20) {
-    ch2 = 0;
+}
+
+void updateIndicators() {
+  if (getBatteryStatus() || !roboclawConnected) {
+    setBuzzer(0, 0);
+  } else {
+    setBuzzer(1260, 200);
   }
-  if (ch3 > -20 && ch3 < 20) {
-    ch3 = 0;
+}
+
+void updateBatteryVoltage() {
+  batteryVoltage = roboclaw.ReadMainBatteryVoltage(RC1, &roboclawConnected);
+}
+
+bool getRadioStatus() {
+  if (radioInitialized && sBus.failsafe_status != 3) {
+    return true;
+  } else {
+    return false;
   }
-  if (ch4 > -20 && ch4 < 20) {
-    ch4 = 0;
+}
+
+bool getBatteryStatus() {
+  unsigned long currentMillis = millis();
+  if (!roboclawConnected) {
+    return false;
+  } else if (batteryVoltage <= batteryAlarmVoltage) {
+    if (currentMillis - batteryAlarmPreviousMillis >= batteryStatusDelay) {
+      batteryResetPreviousMillis = currentMillis;
+      return false;
+    } else {
+      return true;
+    }
+  } else {
+    if (currentMillis < batteryStatusDelay || currentMillis - batteryResetPreviousMillis >= batteryStatusDelay) {
+      batteryAlarmPreviousMillis = currentMillis;
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
@@ -152,167 +232,19 @@ void updateDisplay() {
   }
 }
 
-void failSafe() {
-//  sBus.channels[0] = failsafeChannels[0]; //ch1
-//  sBus.channels[1] = failsafeChannels[1]; //ch2
-//  sBus.channels[2] = failsafeChannels[2]; //ch3
-//  sBus.channels[3] = failsafeChannels[3]; //ch4
-//  sBus.channels[4] = failsafeChannels[4]; //ch5
-//  sBus.channels[5] = failsafeChannels[5]; //ch6
-//  sBus.channels[6] = failsafeChannels[6]; //ch7
-//  sBus.channels[7] = failsafeChannels[7]; //ch8
-}
-
-void initiallizeChannels() {
-  ch1 = 0;
-  ch2 = 0;
-  ch3 = 0;
-  ch4 = 0;
-  ch5 = switchMiddle;
-  ch6 = switchDown;
-  ch7 = switchMiddle;
-  ch8 = switchMiddle;
-}
-
-void setBuzzer(bool condition) {
-  if (condition) {
-    unsigned long currentMillis = millis();
-    if (currentMillis - buzzerPreviousMillis >= buzzerInterval) {
-      buzzerPreviousMillis = currentMillis;
-      if (buzzerOn) {
-        noTone(buzzer);
-        buzzerOn = false;
-      } else {
-        tone(buzzer, 1260);
-        buzzerOn = true;
-      }
-    }
-  } else if (ch8 == switchUp){
-    unsigned long currentMillis = millis();
-    if (currentMillis - buzzerPreviousMillis >= buzzerInterval) {
-      buzzerPreviousMillis = currentMillis;
-      if (buzzerOn) {
-        noTone(buzzer);
-        buzzerOn = false;
-      } else {
-        tone(buzzer, sBus.channels[6]);
-        buzzerOn = true;
-      }
-    }
-  } else {
-    noTone(buzzer);
-  }
-}
-
-void updateIndicators() {
-  if (batteryStatus()) {
-    setBuzzer(false);
-//    if (radioStatus()) {
-//      setRGB(0, 255, 0, 0);
-//    } else {
-//      setRGB(0, 255, 0, 500);
-//    }
-  } else if (!roboclawConnected) {
-    setBuzzer(false);
-//    if (radioStatus()) {
-//      setRGB(40, 255, 0, 0);
-//    } else {
-//      setRGB(40, 255, 0, 500);
-//    }
-  } else {
-    setBuzzer(true);
-//    if (radioStatus()) {
-//      setRGB(255, 0, 0, 0);
-//    } else {
-//      setRGB(255, 0, 0, 500);
-//    }
-  }
-}
-
-bool radioStatus() {
-  if (radioInitialized && sBus.failsafe_status != 3) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-void updateBatteryVoltage() {
-  batteryVoltage = roboclaw.ReadMainBatteryVoltage(RC1, &roboclawConnected);
-}
-
-bool batteryStatus() {
-  updateBatteryVoltage();
-  unsigned long currentMillis = millis();
-  if (!roboclawConnected) {
-    return false;
-  } else if (batteryVoltage <= batteryAlarmVoltage) {
-    if (currentMillis - batteryAlarmPreviousMillis >= batteryStatusDelay) {
-      batteryResetPreviousMillis = currentMillis;
-      return false;
-    } else {
-      return true;
-    }
-  } else {
-    if (currentMillis < batteryStatusDelay || currentMillis - batteryResetPreviousMillis >= batteryStatusDelay) {
-      batteryAlarmPreviousMillis = currentMillis;
-      return true;
-    } else {
-      return false;
-    }
-  }
-}
-
-void drive() {
-  int drive = (int)(ch2 / 5.16);
-  if (drive >= 0) {
-    roboclaw.ForwardM1(RC2, drive);
-  } else {
-    roboclaw.BackwardM1(RC2, -drive);
-  }
-}
-
-//void setRGB(int red, int green, int blue, unsigned long blinkInterval)
-//{
-//  unsigned long currentMillis = millis();
-//  
-//  if (blinkInterval == 0) {
-//    analogWrite(redPin, red);
-//    analogWrite(greenPin, green);
-//    analogWrite(bluePin, blue);
-//  } else if (currentMillis - rgbPreviousMillis >= blinkInterval) {
-//    rgbPreviousMillis = currentMillis;
-//    if (rgbOn) {
-//      analogWrite(redPin, 0);
-//      analogWrite(greenPin, 0);
-//      analogWrite(bluePin, 0);
-//      rgbOn = false;
-//    } else {
-//      analogWrite(redPin, red);
-//      analogWrite(greenPin, green);
-//      analogWrite(bluePin, blue);
-//      rgbOn = true;
-//    }
-//  }
-//}
-
 void displayChannels() {
   display.clearDisplay();
   display.setTextColor(WHITE);
   display.setCursor(0,0);
   for (int i = 0; i < 8; i = i + 2) {
-    display.setCursor(0, 8 * i);
-    display.setTextSize(1);
-    display.print(i + 1);
-    display.print(":");
-    display.setTextSize(2);
-    display.print(sBus.channels[i]);
-    display.setCursor(67, 8 * i);
-    display.setTextSize(1);
-    display.print(i + 2);
-    display.print(":");
-    display.setTextSize(2);
-    display.print(sBus.channels[i + 1]);
+    for (int j = 0; j < 2; j++) {
+      display.setCursor(67 * j, 8 * i);
+      display.setTextSize(1);
+      display.print(i + j + 1);
+      display.print(":");
+      display.setTextSize(2);
+      display.print(channels[i + j]);
+    }
   }
   display.display();
 }
